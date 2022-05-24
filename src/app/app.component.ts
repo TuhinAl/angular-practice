5cb940577b8282055ceb70dbcecdfc5d4841a371
Tuhin <alauddintuhin1411@gmail.com>
2022-05-24 17:57:47 +0600

    reactive form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -14,33 +14,41 @@ export class AppComponent {
     console.warn(this.profileForm.value);
   }
 
-  // profileForm  = new FormGroup({
-  //   firstName : new FormControl(''),
-  //   lastName: new FormControl(''),
-  //   email: new FormControl(''),
-  //   hobbies: new FormControl(''),
-  //   address: new FormGroup({
-  //     street: new FormControl(''),
-  //     city: new FormControl(''),
-  //     state: new FormControl(''),
-  //     zip: new FormControl('')
-  //   })
-  // });
+  profileForm  = new FormGroup({
+    firstName : new FormControl('', [Validators.required, 
+      Validators.minLength(3)]),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl(''),
+    hobbies: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl('')
+    })
+  });
+
+
+  
+
+  get first(){
+    return this.profileForm.get('firstName');
+  }
 
   // using form control
-  profileForm = this.formbuilder.group({
-    firstName: [''],
-    lastName: [''],
-    email: [''],
-    hobbies: [''],
-    address: this.formbuilder.group({
-      street: [''],
-      city: [''],
-      state: [''],
-      zip: ['']
-    }),
+  // profileForm = this.formbuilder.group({
+  //   firstName: ['', Validators.required, Validators.maxLength],
+  //   lastName: [''],
+  //   email: [''],
+  //   hobbies: [''],
+  //   address: this.formbuilder.group({
+  //     street: [''],
+  //     city: [''],
+  //     state: [''],
+  //     zip: ['']
+  //   }),
 
-  });
+  // });
 
 }
 
