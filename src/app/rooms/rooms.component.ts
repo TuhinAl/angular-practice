3a68dd3168dcfec217fe7a87f978f6231ea9639a
tuhinal <alauddintuhin1411@gmail.com>
2022-09-14 23:20:36 +0600

    hands on component create

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'tuhin',
+    templateUrl:'./rooms.component.html',
+    styleUrls:['./rooms.component.css']
+})
+export class RoomsComponent{
+    constructor(){
+
+    }
+}
